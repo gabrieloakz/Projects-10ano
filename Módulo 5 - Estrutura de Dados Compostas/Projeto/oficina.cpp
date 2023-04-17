57cf8fb752e682ae34f21436e3358dc5c877199c
gabrieloakz <ferreiragabriel0193@gmail.com>
2023-04-17 22:14:20 +0100

    progresso no prohjeto

--- Módulo 5 - Estrutura de Dados Compostas/Projeto/oficina.cpp
+++ Módulo 5 - Estrutura de Dados Compostas/Projeto/oficina.cpp
@@ -1,55 +1,115 @@
 // Feito por Gabriel Ferreira Carvalho 10E TGPSI AEB
 
-// Descrição:Pretende-se construir um programa que faça a gestão de clientes e 
+// Descrição:Pretende-se construir um programa que faça a gestão de clientes e
 // respetivos automóveis da oficina “CarRepair”, oficina de reparações de automóveis.
-
 
 #include <iostream>
 #include <string>
 #include <locale>
 using namespace std;
 
-int menu_principal() 
+const int MAX_CLIENTE = 20;
+
+struct Cliente
 {
 
-system("cls");
+    int idCliente;
 
-puts("======== MENU PRINCIPAL ========");
+    string nome;
 
-puts("\n\t1 - Menu Clientes");
+    string email;
 
-puts("\n\t1 - Menu Automóveis");
+    int tlm;
+};
 
-puts("\n\t1 - Pesquisas");
+Cliente clientes[MAX_CLIENTE] = {
+    {1, "João Pinto", "j.pint@gmail.com", 912352513},
+    {2, "Ana Sousa", "a na_sousa@gmail.com", 913357788},
 
-puts("\n\n\t0 - Sair");
+};
 
-puts("================================\n");
+int menu_principal()
+{
+    int Opcao_Escolhida;
 
-cout << "Opção:";
+    system("cls");
 
-cin >> int Opcao_Escolhida;
+    puts("======== MENU PRINCIPAL ========");
 
-return Opcao_Escolhida;
+    puts("\n\t1 - Menu Clientes");
 
+    puts("\n\t2 - Menu Automóveis");
 
+    puts("\n\t3 - Pesquisas");
+
+    puts("\n\n\t0 - Sair");
+
+    puts("\n================================\n");
+
+    cout << "Opção:";
+
+    cin >> Opcao_Escolhida;
+
+    return Opcao_Escolhida;
+}
+
+int menu_clientes()
+{
+
+    int Opcao_Escolhida;
+
+    system("cls");
+
+    _sleep(400);
+
+    puts("================ MENU CLIENTES ================");
+
+    puts("\n\t1 - Inserir Clientes");
+
+    puts("\n\t2 - Ver Todos os Clientes");
+
+    puts("\n\t3 - Mostrar Dados do cliente pelo código");
+
+    puts("\n\n\t0 - Sair");
+
+    puts("\n===========================================\n");
+
+    cout << "Opção:";
+
+    cin >> Opcao_Escolhida;
+
+    return Opcao_Escolhida;
 }
 
 int main(int argc, char const *argv[])
 {
     setlocale(LC_ALL, "pt_PT.utf8");
-    
+
     system("color 0e");
-    
+
     int opcao;
 
     do
     {
         opcao = menu_principal();
 
+        switch (opcao)
+        {
+
+        case 1:
+            menu_clientes();
+            break;
+
+        default:
+            puts("\nERRO:Inseriu uma opção inválida.\nTente novamente.\n\n");
+            system("pause");
+            opcao = -1;
+            break;
+        }
+
     } while (opcao = 0);
 
-    cout << "Obrigado por usar o programa. Até à proxima.\m\n";
+    cout << "\nObrigado por usar o programa. Até à proxima.\n\n";
 
     return 0;
 }
